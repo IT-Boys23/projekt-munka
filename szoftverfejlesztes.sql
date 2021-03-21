94922732db175a1564c69843c361723db9c36c92
IT-Boys23 <79209121+IT-Boys23@users.noreply.github.com>
2021-03-21 15:07:54 +0100

    Adatbázis

---
+++ szoftverfejlesztes.sql
@@ -0,0 +1,56 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Gép: 127.0.0.1:3306
+-- Létrehozás ideje: 2021. Már 21. 14:07
+-- Kiszolgáló verziója: 5.7.31
+-- PHP verzió: 7.3.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Adatbázis: `szoftverfejlesztes`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `felhasznalok`
+--
+
+DROP TABLE IF EXISTS `felhasznalok`;
+CREATE TABLE IF NOT EXISTS `felhasznalok` (
+  `nev` varchar(50) COLLATE utf8mb4_hungarian_ci NOT NULL,
+  `becenev` varchar(15) COLLATE utf8mb4_hungarian_ci NOT NULL,
+  `neptunkod` varchar(6) COLLATE utf8mb4_hungarian_ci NOT NULL,
+  `jelszo` varchar(20) COLLATE utf8mb4_hungarian_ci NOT NULL,
+  `kar` varchar(10) COLLATE utf8mb4_hungarian_ci NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `orarend`
+--
+
+DROP TABLE IF EXISTS `orarend`;
+CREATE TABLE IF NOT EXISTS `orarend` (
+  `nap` varchar(15) COLLATE utf8mb4_hungarian_ci NOT NULL,
+  `oranev` varchar(50) COLLATE utf8mb4_hungarian_ci NOT NULL,
+  `kezdoido` time(6) NOT NULL,
+  `epulet` text COLLATE utf8mb4_hungarian_ci NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
