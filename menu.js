240e5884698faa2831efc0fa96f766134c9e0d7c
IT-Boys23 <79209121+IT-Boys23@users.noreply.github.com>
2021-03-28 15:11:12 +0200

    design

--- menu.js
+++ menu.js
@@ -2,24 +2,24 @@ var activeOffset = $("nav ul .active").position().left;
 var activeItemWidth = $("nav ul .active").width();
 
 nav("document").ready(function () {
-	$(".dot").css("left", activeOffset + activeItemWidth / 2);
+	nav(".dot").css("left", activeOffset + activeItemWidth / 2);
 	var bgColor = $(".active a").css("background-color");
-	$(".dot").css("background-color", bgColor);
+	nav(".dot").css("background-color", bgColor);
 });
 
 nav("nav").mouseout(function () {
-	$(".dot").css("left", activeOffset + activeItemWidth / 2);
+	nav(".dot").css("left", activeOffset + activeItemWidth / 2);
 	var bgColor = $(".active a").css("background-color");
-	$(".dot").css("background-color", bgColor);
+	nav(".dot").css("background-color", bgColor);
 });
 
 nav("nav ul li").hover(function () {
 	var navOffset = $(this).position().left;
 	var navItemWidth = $(this).width();
 
-	$(".dot").css("left", navOffset + navItemWidth / 2);
+	nav(".dot").css("left", navOffset + navItemWidth / 2);
 
 	var bgColor = $("a", this).css("background-color");
 
-	$(".dot").css("background-color", bgColor);
+	nav(".dot").css("background-color", bgColor);
 });
