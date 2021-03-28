8d136aa720e34cf4e3f65a54b5ed2c865dba61d4
IT-Boys23 <79209121+IT-Boys23@users.noreply.github.com>
2021-03-28 14:51:23 +0200

    Format

--- menu.js
+++ menu.js
@@ -1,19 +1,19 @@
 var activeOffset = $("nav ul .active").position().left;
 var activeItemWidth = $("nav ul .active").width();
 
-$("document").ready(function () {
+nav("document").ready(function () {
 	$(".dot").css("left", activeOffset + activeItemWidth / 2);
 	var bgColor = $(".active a").css("background-color");
 	$(".dot").css("background-color", bgColor);
 });
 
-$("nav").mouseout(function () {
+nav("nav").mouseout(function () {
 	$(".dot").css("left", activeOffset + activeItemWidth / 2);
 	var bgColor = $(".active a").css("background-color");
 	$(".dot").css("background-color", bgColor);
 });
 
-$("nav ul li").hover(function () {
+nav("nav ul li").hover(function () {
 	var navOffset = $(this).position().left;
 	var navItemWidth = $(this).width();
 
