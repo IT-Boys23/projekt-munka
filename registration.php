95e1497aef822f79189a1de807d913b4d70e6f5a
IT-Boys23 <79209121+IT-Boys23@users.noreply.github.com>
2021-05-01 10:29:40 +0200

    .

--- registration.php
+++ registration.php
@@ -8,9 +8,9 @@
     $sql = "INSERT INTO szemelyek VALUES('$nev','$becenev','$neptun','$jelszo')";
     mysqli_set_charset($conn,"utf8");
     if($conn->query($sql)===true){
-        echo "Sor siekresen hozzáadva! <br> <a href='alap.php'>Vissza a főoldalra</a>";
+        echo "Sikeres regisztráció! <br> ";
     }else{
-        echo "Nem sikerült hozzáadni a sort!";
+        echo "Sikertelen regisztráció!";
     }
     $conn->close();
 ?>
