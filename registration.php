28e45e4ac5ff0759d0a3bf8c75510c2c1f54032f
IT-Boys23 <79209121+IT-Boys23@users.noreply.github.com>
2021-05-01 10:24:44 +0200

    regisztració

--- registration.php
+++ registration.php
@@ -1,25 +1,16 @@
 <?php
-			mb_internal_encoding("utf-8");
-			$servername = "localhost";
-			$username = "root";
-			$password = "";
-			$dbname = "szoftverfejltesztes";
-			// Create connection
-			$conn = new mysqli($servername, $username, $password, $dbname);
-			// Check connection
-			if ($conn->connect_error) {
-			die("Connection failed: " . $conn->connect_error);
-			}
-			$sql = "SELECT Nev, SzuletesiIdo, Foglalkozas, HaviKereset, SzemIgSzam FROM szemelyek";
-			mysqli_set_charset($conn,"utf8");
-			$result = $conn->query($sql);
-			if ($result->num_rows > 0) 
-			{
-				// output data of each row
-				while($row = $result->fetch_assoc()) 
-				{
-					print('<tr><td>'.$row["Nev"].'</td><td>'.$row["SzuletesiIdo"].'</td><td>'.$row["Foglalkozas"].'</td><td>'.$row["HaviKereset"].'</td><td>'.$row["SzemIgSzam"].'</td><td><button onclick=modosit("'. $row["SzemIgSzam"] .'")>Modosit</button><button onclick=torles("'. $row['SzemIgSzam'] .'")>Töröl</button></td></tr>');
-				}
-			}
-			$conn->close();
-		?>
+    include "connection.php";
+    $nev = $_POST['name'];
+    $becenev = $_POST['nick'];
+    $neptun = $_POST['neotun'];
+    $jelszo = $_POST['psw'];
+
+    $sql = "INSERT INTO szemelyek VALUES('$nev','$becenev','$neptun','$jelszo')";
+    mysqli_set_charset($conn,"utf8");
+    if($conn->query($sql)===true){
+        echo "Sor siekresen hozzáadva! <br> <a href='alap.php'>Vissza a főoldalra</a>";
+    }else{
+        echo "Nem sikerült hozzáadni a sort!";
+    }
+    $conn->close();
+?>
--- szoftverfejlesztes.sql
+++ szoftverfejlesztes.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1:3306
--- Létrehozás ideje: 2021. Már 28. 13:16
+-- Létrehozás ideje: 2021. Máj 01. 08:19
 -- Kiszolgáló verziója: 5.7.31
 -- PHP verzió: 7.3.21
 
@@ -32,8 +32,7 @@ CREATE TABLE IF NOT EXISTS `felhasznalok` (
   `nev` varchar(50) COLLATE utf8mb4_hungarian_ci NOT NULL,
   `becenev` varchar(15) COLLATE utf8mb4_hungarian_ci NOT NULL,
   `neptunkod` varchar(6) COLLATE utf8mb4_hungarian_ci NOT NULL,
-  `jelszo` varchar(20) COLLATE utf8mb4_hungarian_ci NOT NULL,
-  `kar` varchar(10) COLLATE utf8mb4_hungarian_ci NOT NULL
+  `jelszo` varchar(20) COLLATE utf8mb4_hungarian_ci NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
 
 -- --------------------------------------------------------
