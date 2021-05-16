b0f373c2e496539ee93669f7025f761be22a0bec
Rentler Ferenc <79209121+IT-Boys23@users.noreply.github.com>
2021-05-16 17:04:57 +0200

    látnivalók

--- Nev.php
+++ Nev.php
@@ -1,26 +1,72 @@
 <html>
 <body>
-    <h1>Nevezetességek</h1>
+    <h1>Látnivalók</h1>
 
 
 
   <div class="nevezetessegek">
-    <img src="Images/szobor1.jpeg" width="300" height="300"  align="justify"> Somogyi Árpád Ülő nő kőszobrát 1961 óta látható a "B" épület előtt.
+    <img src="Images/szobor1.jpeg" width="350" height="auto"  > Somogyi Árpád Ülő nő kőszobrát 1961 óta látható a "B" épület előtt.
     <p><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2716.608030301469!2d17.906345615252075!3d47.087149731709886!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47699a6c3c4d1c35%3A0xe6dd5f792ed6576c!2sPannon%20Egyetem%20M%C3%A9rn%C3%B6ki%20Kar!5e0!3m2!1shu!2shu!4v1617725988666!5m2!1shu!2shu" ></iframe></iframe></p>
     </div>
-            
-    <img src="Images/szobor2.jpeg" >
-            
+       
+    <div class="nevezetessegek"><img src="Images/szobor2.jpeg"  width="350" height="auto" >
     A "B" épületben álló Minerva bronzszobor Lugossy László alkotása. A Magyar Tudomány Napján, 2000-ben avatták fel.
+    <p><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2716.608030301469!2d17.906345615252075!3d47.087149731709886!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47699a6c3c4d1c35%3A0xe6dd5f792ed6576c!2sPannon%20Egyetem%20M%C3%A9rn%C3%B6ki%20Kar!5e0!3m2!1shu!2shu!4v1617725988666!5m2!1shu!2shu" ></iframe></iframe></p>
 
-
-
-          
-   <img src="Images/szobor3.jpg" >
+</div>
+    <div class="nevezetessegek"><img src="Images/szobor3.jpg"  width="350" height="auto">
     Az 1956 emlékére készült bronz örökmécses Raffay Béla munkája, és a "B" épület falát ékesíti.
+    <p><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2716.608030301469!2d17.906345615252075!3d47.087149731709886!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47699a6c3c4d1c35%3A0xe6dd5f792ed6576c!2sPannon%20Egyetem%20M%C3%A9rn%C3%B6ki%20Kar!5e0!3m2!1shu!2shu!4v1617725988666!5m2!1shu!2shu" ></iframe></iframe></p>
 
+  </div>
+    <div class="nevezetessegek"><img src="Images/varhegy.jpg" width="350" height="auto"  >
+    Veszprémi várhegy.
+    <p><iframe src="<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2716.1285473806506!2d17.901294016323835!3d47.09655057915412!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47699a5dafc2bf35%3A0xd480ee02c6982856!2zVmVzenByw6ltaSBWw6Fy!5e0!3m2!1shu!2shu!4v1621177268069!5m2!1shu!2shu" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>" ></iframe></iframe></p>
 
+  </div>
+    <div class="nevezetessegek"><img src="Images/tuztorony.jpg" width="350" height="auto"  >
+    Veszprémi tűztorony.
+    <p><iframe src="<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2716.219447623937!2d17.903452916323772!3d47.094768479154126!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47699a678d874c8d%3A0x2ec074a16dfbb775!2sT%C5%B1ztorony!5e0!3m2!1shu!2shu!4v1621177283794!5m2!1shu!2shu" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>" ></iframe></iframe></p>
 
+  </div>
+    <div class="nevezetessegek"><img src="Images/szentistv.jpg" width="350" height="auto"  >
+    Szent István és Gizella szobra
+    <p><iframe src="<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2716.0812880093763!2d17.900230516323994!3d47.09747707915438!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47699a5c3822caad%3A0x3f979bd648fa88eb!2sSzent%20Istv%C3%A1n%20kir%C3%A1ly%20%C3%A9s%20Boldog%20Gizella%20kir%C3%A1lyn%C3%A9%20szobra!5e0!3m2!1shu!2shu!4v1621177314283!5m2!1shu!2shu" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>" ></iframe></iframe></p>
+
+  </div>
+    <div class="nevezetessegek"><img src="Images/hosok.jpg" width="350" height="auto"  >
+    Hősők kapuja Veszprém.
+    <p><iframe src="<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2716.1986215447073!2d17.903455516323795!3d47.09517677915415!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47699a678fe678af%3A0xeb273ced41ad2c32!2zVmVzenByw6ltLCBIxZFzw7ZrIGthcHVqYSwgVsOhciB1LiAyLCA4MjAw!5e0!3m2!1shu!2shu!4v1621177338106!5m2!1shu!2shu" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>" ></iframe></iframe></p>
+
+  </div>
+  <div class="nevezetessegek"><img src="Images/ovaros.jpg." width="350" height="auto"  >
+      Óváros tér.
+    <p><iframe src="<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2716.2459250582015!2d17.904018516323884!3d47.09424937915408!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47699a67c09e9275%3A0xd37a0e9348ed0f3b!2zVmVzenByw6ltLCDDk3bDoXJvcyB0w6lyLCA4MjAw!5e0!3m2!1shu!2shu!4v1621177374889!5m2!1shu!2shu" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>" ></iframe></iframe></p>
+
+    </div>
+    <div class="nevezetessegek"><img src="Images/gizellakilato.jpeg" width="350" height="auto"  >
+    Gizella Királyné Kilátó
+    <p><iframe src="<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2716.207282517268!2d17.884520416323806!3d47.09500697915401!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47699a4fb52e9a93%3A0xe14a406d0472caf0!2zR2l6ZWxsYSBLaXLDoWx5bsOpIEtpbMOhdMOz!5e0!3m2!1shu!2shu!4v1621177394714!5m2!1shu!2shu" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>" ></iframe></iframe></p>
+
+  </div>
+    <div class="nevezetessegek"><img src="Images/cannyon.jpg" width="350" height="auto"  >
+    Grand Canyon pub veszprém.
+    <p><iframe src="<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2716.32678416066!2d17.90823141632378!3d47.09266407915397!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47699a66077f6c6f%3A0xc4cfaff280a8afa8!2zR3JhbmQgQ2FueW9uIFPDtnLDtnrFkQ!5e0!3m2!1shu!2shu!4v1621177423877!5m2!1shu!2shu" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>" ></iframe></iframe></p>
+
+  </div>
+    <div class="nevezetessegek"><img src="Images/diploma.jpg" width="350" height="auto"  >
+    DiplomaFosztó Klub
+    <p><iframe src="<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2716.684716148023!2d17.906489216323557!3d47.08564607915358!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47699b2805f2d5d1%3A0x6a2934a65d5e63ee!2sDiplomaFoszt%C3%B3%20Klub!5e0!3m2!1shu!2shu!4v1621177441457!5m2!1shu!2shu" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>" ></iframe></iframe></p>
+
+  </div>
+    <div class="nevezetessegek"> 
+    <img src="Images/geszti.jpg" width="350" height="auto"  >
+    Gesztenyés Sörház.
+    <p><iframe src="<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2716.4185610321747!2d17.899405116323845!3d47.090864679153995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47699a4279017d7d%3A0x38e16de2338e946a!2zR2VzenRlbnnDqXMgU8O2csO2esWR!5e0!3m2!1shu!2shu!4v1621177172118!5m2!1shu!2shu" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>" ></iframe></iframe></p>
+
+    /div>
+
+    
     </body>
 
 </html>
