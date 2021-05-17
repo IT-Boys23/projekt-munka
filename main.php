419b3126a65903cd2dc1bf33b51f3b4585f8e824
birkaszsofia <79933018+birkaszsofia@users.noreply.github.com>
2021-05-17 16:48:39 +0200

    szoveg

--- main.php
+++ main.php
@@ -1,2 +1,7 @@
-<h1>ÜDvözlünk a United Unies weboldalán!</h1>
-<h2>Rólunk:</h2>
+<h1>Üdvözlünk a United Unies weboldalán!</h1>
+<p></p>
+<h3>Amikor bekerül valaki az egyetemre, egy csomó idegen emberrel, épülettel és kifejezéssel találkozik mindenki. Ha csinálnánk egy közvéleménykutatást az első napokban hogy ki tudja melyik az A01-es terem, nagy összeget tennénk rá hogy sokan csak vennének egy nagy levegőt.  </h3>
+<p></p>
+<h3> A sok kérdezősködés és aggódás helyett mi szeretnénk nektek segíteni, és egy helyre összegyűjteni azokat  a dolgokat amikre szükségetek lehet az egyetemi pályafutásotok alatt. </h3>
+<p></p>
+<h2></h2>
