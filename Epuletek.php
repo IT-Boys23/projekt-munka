ef1466894e0bbab049c53262780f7078cb5db947
Rentler Ferenc <79209121+IT-Boys23@users.noreply.github.com>
2021-05-18 18:56:53 +0200

    .

--- Epuletek.php
+++ Epuletek.php
@@ -1,6 +1,4 @@
-<html>
-<<<<<<< Updated upstream
-	
+<html>	
 <style>
 .kep{
     width: 350px;
@@ -14,12 +12,6 @@
 }
 </style>
 
-=======
-    <style>
-        
-    </style>
-<link href="design.css" rel="stylesheet" type="text/css">
->>>>>>> Stashed changes
 <body>
 
     <h1>Egyetemi épületek </h1>
@@ -31,7 +23,7 @@
 	<p><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d695471.524676649!2d17.34925986562499!3d47.085627899999984!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47699a6d0388e1d7%3A0x123c0ebbd13e03a5!2sPannon%20Egyetem%20K%C3%B6zponti%20Koll%C3%A9gium!5e0!3m2!1shu!2shu!4v1621184011107!5m2!1shu!2shu" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe></p>
     </div>
     <h3>Hotel Magister </h3>
-    <img class="kep" src="Images/hm.jpg"
+    <img class="kep" src="Images/hm.jpg">
     <div class="post-content">
 	<p><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d174136.9861114401!2d17.850786287715714!3d47.00312478196659!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47699a6a6a41f3b7%3A0xa58806c9b682696c!2sHostel%20Magister!5e0!3m2!1shu!2shu!4v1621184971056!5m2!1shu!2shu" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe></p>
     </div>
@@ -40,23 +32,23 @@
 
     <h1> Épületek </h1>
 
-    <img class="kep" src="Images/mik.png">
+    <img  src="Images/mik.png" width="70%">
     <div class="post-content">
 	<p><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2716.488845370345!2d17.90581991525223!3d47.089486631549796!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47699a693492be33%3A0xb041338ca1ff6cf9!2sM%C5%B1szaki%20Informatikai%20Kar!5e0!3m2!1shu!2shu!4v1617723004615!5m2!1shu!2shu" ></iframe></iframe></p>
     </div>
-    <img class="kep" src="Images/mikv.png">
+    <img src="Images/mikv.png"width="90%">
 
-    <img class="kep" src="Images/gtk.png">
+    <img  src="Images/gtk.png" width="70%">
     <div class="post-content">
     <p><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2716.489105481886!2d17.90576981525221!3d47.08948153155018!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47699a6eae350c1f%3A0x9fa7dc38cc000fbf!2sPannon%20Egyetem%20Gazdas%C3%A1gtudom%C3%A1nyi%20Kar!5e0!3m2!1shu!2shu!4v1617725611322!5m2!1shu!2shu" ></iframe></p>
     </div>
-    <img class="kep" src="Images/gtkv.png">
+    <img  src="Images/gtkv.png">
            
-    <img src="Images/mk.png">
+    <img src="Images/mk.png" width="70%">
     <div class="post-content">
     <p><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2716.608030301469!2d17.906345615252075!3d47.087149731709886!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47699a6c3c4d1c35%3A0xe6dd5f792ed6576c!2sPannon%20Egyetem%20M%C3%A9rn%C3%B6ki%20Kar!5e0!3m2!1shu!2shu!4v1617725988666!5m2!1shu!2shu" ></iframe></iframe></p>
     </div>
-    <img class="kep" src="Images/mkv.png">	
+    <img  src="Images/mkv.png"width="90%">	
     
 </body>
 </html>
